5b1066964e2404e4b0ecb3962ef9585854100f77
Elijoon93 <abtin412@gmail.com>
2026-07-28 14:33:39 +0330

    Refresh PWA cache for v8.1 Semester and Research Pack

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,5 @@
-const CACHE='lgmk-v8-academic-content-20260728';
-const ASSETS=['./','./index.html','./assets/css/style.css?v=800','./assets/js/app.js?v=800','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
-  }).catch(()=>caches.match(event.request).then(match=>match||caches.match('./index.html'))));
-});
+const CACHE='lgmk-v8-1-semester-research-20260728';
+const ASSETS=['./','./index.html','./assets/css/style.css?v=810','./assets/js/data-core.js?v=810','./assets/js/data-language.js?v=810','./assets/js/data-learning.js?v=810','./assets/js/data-merge.js?v=810','./assets/js/app-core.js?v=810','./assets/js/app-views1.js?v=810','./assets/js/app-views2.js?v=810','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html'))))});
